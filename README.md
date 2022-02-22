fc0f647067f7d34e8ba9f310161efcadd8ca4be4
Matthew Smith <marioandweegee3@gmail.com>
2022-02-22 01:04:18 -0500

    Add files from local

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tecc-3
+A tech-based, kitchen-sink pack for Minecraft Fabric 1.18
---
+++ kubejs/server_scripts/script.js
@@ -0,0 +1,23 @@
+// priority: 0
+
+settings.logAddedRecipes = true
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+onEvent('recipes', event => {
+	event.shaped('16x minecraft:stick', [
+		'L',
+		'L',
+	], {
+		L: '#minecraft:logs',
+	})
+})
+
+onEvent('item.tags', event => {
+	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
+	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
+
+	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
+	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
+})
